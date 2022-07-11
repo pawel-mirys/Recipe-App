1a5e3d27ea1e502cbd9481edbcdebf7d3eb592e5
Paweł Mirys <55873454+pawel-mirys@users.noreply.github.com>
2022-07-11 16:45:51 +0200

    Add init files - HomePage and Button

---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,25 @@
+import styles from './Home.module.scss';
+import { Button } from 'ui/Button/Button';
+export const Home = () => {
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.pageContainer}>
+        <div className={styles.headerContainer}>
+          <h1>The Recipe App</h1>
+          <p>What recipe do you want to save today?</p>
+        </div>
+        <div className={styles.searchContainer}>
+          <input className={styles.searchInput} type="text" placeholder="Search" />
+        </div>
+        <div className={styles.recipesContainer}></div>
+      </div>
+      <Button
+        onClick={() => {
+          console.log('sfsa');
+        }}
+      >
+        Add Recipe
+      </Button>
+    </div>
+  );
+};
