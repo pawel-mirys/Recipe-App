d3aa28f7577e41f6f9c77367077de4fcb1826d52
Paweł Mirys <55873454+pawel-mirys@users.noreply.github.com>
2022-09-16 20:23:13 +0200

    Add Preview Link

--- src/app/Components/RecipesList/RecipesList.tsx
+++ src/app/Components/RecipesList/RecipesList.tsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { RecipeContext } from 'app/contexts/RecipeContext';
 import styles from './RecipesList.module.scss';
 import { Recipe } from '../Recipe/Recipe';
+import { LinkButton } from 'ui/LinkButton/LinkButton';
 
 export const RecipesList = () => {
   const recipeContext = useContext(RecipeContext);
@@ -10,12 +11,14 @@ export const RecipesList = () => {
   let listItem = recipeContext?.recipesList.map((item, index) => {
     return (
       <div className={styles.listItem} key={index}>
-        <Recipe
-          title={item.title}
-          description={item.description}
-          ingredients={item.ingredients}
-          id={`Recipe No. ${index + 1}`}
-        />
+        <LinkButton to="/preview" className={styles.previewLink}>
+          <Recipe
+            title={item.title}
+            description={item.description}
+            ingredients={item.ingredients}
+            id={`Recipe No. ${index + 1}`}
+          />
+        </LinkButton>
       </div>
     );
   });
--- src/app/router/RouterSwitch.tsx
+++ src/app/router/RouterSwitch.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 import { Home } from 'app/pages/Home/Home';
 import { RecipeCreator } from 'app/pages/RecipeCreator/RecipeCreator';
+import { RecipePreview } from 'app/pages/RecipePreview/RecipePreview';
 
 export const RouterSwitch = () => {
   return (
     <Routes>
       <Route path={routes.creator} element={<RecipeCreator />} />
+      <Route path={routes.preview} element={<RecipePreview />} />
       <Route path={routes.home} element={<Home />} />
     </Routes>
   );
